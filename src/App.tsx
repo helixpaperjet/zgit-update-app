d32d09aea38d662bd0442e350d80c492acc3a1a6
imsonmia <imsonmia@outlook.com>
2022-03-16 22:24:52 +0200

    implemented key feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,24 @@
 import React from 'react';
 import './App.css';
 import { UpdateData } from './classes';
-let currentObjJSON: string;
 let key_: string = "HG4DJA55CLML4DNH";
-/*
-function composeChildElementsFromUpdate(update: UpdateData) {
-  let result;
-  const data = update.data;
-  const history = data.data;
-  const datum = history[5];
-  let updateElements: JSX.Element;
-  history.forEach((element) => {
-    let eachElement = 
-    <>
-      <ul>
-        <li>构件号: {element[0]}</li>
-        <li>版本名: {element[1]}</li>
-        <li>版本号: {element[2]}</li>
-        <li>状态: {element[3]}</li>
-        <li>说明: {element[4]}</li>
-        <li>更新数据: {element[5]}</li>
-      </ul>
-    </>
+const getInput = (elementID: string) => {
+  return (document.getElementById(elementID) as HTMLInputElement).value;
+}
+const fetchData = async(url: string) => {
+  return await fetch(url, {mode: "cors", method: "GET"});
+}
+const postData = async(url: string, data: object) => {
+  return await fetch(url, {
+    mode: "cors", 
+    method: "POST", 
+    body: JSON.stringify(data), 
+    headers: 
+    {
+      'Content-Type': 'application/json'
+    }
   })
-} */
+}
 const templateData: UpdateData = 
 {
   "latest": 1,
@@ -61,74 +56,66 @@ class App extends React.Component<{},{data: UpdateData}> {
       data: templateData
     };
   }
-  fetchData(appName: string): boolean {
-    let url = `http://127.0.0.1:8088/data/test/${appName}`;
-    let success: boolean = false;
-    fetch(url, {mode: "cors"})
-    .then((response) => response.json())
-    .then((response) => {
-      this.setState({data: response});
-      success = true;
-    })
-    .catch((reason) => {
-      console.error(reason);
-      success = false;
-    })
-    return success;
-  }
   componentDidMount() {
     document.title = "至远光辉软件更新系统";
     document.getElementById("input-btn")?.addEventListener("click", () => {
-      let value = "UpdateCenter";// document.getElementById("input")?.nodeValue;
+      let value = getInput("get")
       if (typeof value === 'string') {
-        this.fetchData(value);
+        fetchData(`http://127.0.0.1:8088/data/test/${value}`)
+        .then(response => response.json())
+        .then(response => this.setState({data: response}));
         this.render();
       }
-    }
-    
-    )
+    })
     document.getElementById("push-update")?.addEventListener("click", () => {
-      if (true/*key_ === (document.getElementById("key") as HTMLInputElement).value*/) {
-        console.log("ok");
-        
-        if (true) {
-            var obj = this.state.data;
-            var originalLatest = obj.latest;
-            var bn = Number.parseInt((document.getElementById("build-number") as HTMLInputElement).value);
-            var un = (document.getElementById("name") as HTMLInputElement).value;
-            var num = (document.getElementById("number") as HTMLInputElement).value;
-            var stat = (document.getElementById("status") as HTMLInputElement).value;
-            var ud = (document.getElementById("description") as HTMLInputElement).value;
-            var dt = (document.getElementById("download-type") as HTMLInputElement).value;
-            var dd = (document.getElementById("download-data") as HTMLInputElement).value;
-            // new obj.data.data
-            // check if every input box is filled
-            if (bn && un && num && stat && ud && dt && dd) {
-                var updateData = [bn, un, num, stat, ud, { type: dt, data: dd }];
-                console.log(updateData);
-                // check if new build number is wrong
-                if (!(bn <= originalLatest)) {
-                    obj.latest = bn;
-                    if (bn > originalLatest + 1) {
-                        var i = originalLatest + 1;
-                        // add placeholder element for each skipped version
-                        while (i < bn) {
-                            var placeholder = [i, "", "", "", "", { type: "", data: "" }];
-                            console.log(placeholder);
-                            obj.data.data.push(placeholder);
-                            i++;
-                        }
-                    }
-                    obj.data.data.push(updateData);
-                    console.log("Pushed update data");
-                    (document.getElementById("json-export") as HTMLParagraphElement).innerText = JSON.stringify(obj);
+      let key = getInput("key");
+      let projKey;
+      fetchData(`http://127.0.0.1:8088/key/${getInput("get")}`)
+      .then(res => res.text())
+      .then(text => {
+        if (key === text) {
+          console.log("ok");
+          if (true) {
+              var obj = this.state.data;
+              var originalLatest = obj.latest;
+              var bn = Number.parseInt(getInput("build-number"));
+              var un = getInput("name");
+              var num = getInput("number");
+              var stat = getInput("status");
+              var ud = getInput("description");
+              var dt = getInput("download-type");
+              var dd = getInput("download-data");
+              // new obj.data.data
+              // check if every input box is filled
+              if (bn && un && num && stat && ud && dt && dd) {
+                  var updateData = [bn, un, num, stat, ud, { type: dt, data: dd }];
+                  console.log(updateData);
+                  // check if new build number is wrong
+                  if (!(bn <= originalLatest)) {
+                      obj.latest = bn;
+                      if (bn > originalLatest + 1) {
+                          var i = originalLatest + 1;
+                          // add placeholder element for each skipped version
+                          while (i < bn) {
+                              var placeholder = [i, "", "", "", "", { type: "", data: "" }];
+                              console.log(placeholder);
+                              obj.data.data.push(placeholder);
+                              i++;
+                          }
+                      }
+                      obj.data.data.push(updateData);
+                      console.log("Pushed update data");
+                      (document.getElementById("json-export") as HTMLParagraphElement).innerText = JSON.stringify(obj);
+                      postData(`http://127.0.0.1:8088/data/test/${getInput("get")}`, obj)
+                      .then(response => console.log(response))
+                  }
+                }
+                else {
+                  alert("Must fill all boxes")
                 }
               }
-              else {
-                alert("Must fill all boxes")
-              }
             }
-          }
+      });
         }
       )
     }
@@ -136,9 +123,10 @@ class App extends React.Component<{},{data: UpdateData}> {
     const thisState = this.state;
     const element = thisState.data.data.data[thisState.data.latest];
     return (
-    <div className="App">
+      <div className="App">
       <h1>至远光辉软件更新系统 | ZGIT Software Update System Z-SUS</h1>
       <div id="updateInfo">
+        <input id="get"></input>
         <button id="input-btn" className='float-left'>获取更新数据（JSON）</button>
         <fieldset className='original-data-div float-left'>
           <legend><b>原始数据</b></legend>
@@ -157,7 +145,7 @@ class App extends React.Component<{},{data: UpdateData}> {
         </fieldset>
         <fieldset className='new-update-div'>
           <legend><b>输入新数据</b></legend>
-          <div className='float-left'>
+          <div className='float-left' id='new-data-input'>
             <label>构建号：</label><br></br><input id='build-number'></input><br></br>
             <label>版本名：</label><br></br><input id='name'></input><br></br>
             <label>版本号：</label><br></br><input id='number'></input><br></br>
@@ -165,7 +153,7 @@ class App extends React.Component<{},{data: UpdateData}> {
             <label>说明：</label><br></br><input id='description'></input><br></br>
             <label>下载类型：</label><br></br><input id='download-type'></input><br></br>
             <label>下载地址：</label><br></br><input id='download-data'></input><br></br>
-            <button id='push-update'>Push New Update</button>
+            <input id='key'></input><button id='push-update'>Push New Update</button>
           </div>
           <p id='json-export'></p>
         </fieldset>
